76ffde3ac81756d3d83d78d58775b894e814d843
RonRonMonday <58414707+aki7725@users.noreply.github.com>
2024-11-13 13:52:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -579,5 +579,6 @@ async def main():
 
 if __name__ == "__main__":
     #print(pygame.font.get_fonts())
+    print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
